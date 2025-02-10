9c40d25b3bfc175976ba45be2925f69db5c09e88
David Cook <dcook@divviup.org>
2025-02-10 15:53:48 -0600

    chore!: upgrade opentelemetry dependencies

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -1,15 +1,12 @@
 use opentelemetry::global::set_meter_provider;
 use opentelemetry_otlp::MetricExporter;
-use opentelemetry_sdk::{
-    metrics::{PeriodicReader, SdkMeterProvider},
-    runtime::Tokio,
-};
+use opentelemetry_sdk::metrics::{PeriodicReader, SdkMeterProvider};
 use trillium_opentelemetry::Metrics;
 use trillium_router::{router, RouterConnExt};
 
 fn set_up_collector() {
-    let exporter = MetricExporter::builder().with_tonic().build().unwrap();
-    let reader = PeriodicReader::builder(exporter, Tokio).build();
+    let exporter = MetricExporter::builder().with_http().build().unwrap();
+    let reader = PeriodicReader::builder(exporter).build();
     let meter_provider = SdkMeterProvider::builder().with_reader(reader).build();
     set_meter_provider(meter_provider);
 }
